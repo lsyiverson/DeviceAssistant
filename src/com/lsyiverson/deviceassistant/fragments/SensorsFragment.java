6d9c335f13e31d23a2eb6c68c24b2692e11a21f8
lsyiverson <lsyiverson@gmail.com>
2014-01-13 16:05:46 +0800

    Finish the SensorFragment

--- src/com/lsyiverson/deviceassistant/fragments/SensorsFragment.java
+++ src/com/lsyiverson/deviceassistant/fragments/SensorsFragment.java
@@ -20,12 +20,11 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import com.lsyiverson.deviceassistant.R;
+import com.lsyiverson.deviceassistant.utils.BatteryUtils;
 import com.lsyiverson.deviceassistant.utils.Constants;
 import com.lsyiverson.deviceassistant.utils.LogUtils;
 
 public class SensorsFragment extends Fragment implements SensorEventListener {
-
-    protected List<Map<String, Object>> mList = new ArrayList<Map<String, Object>>();
 
     private SimpleAdapter mAdapter;
 
@@ -33,7 +32,11 @@ public class SensorsFragment extends Fragment implements SensorEventListener {
 
     private List<Sensor> mSensorList;
 
-    private ArrayList<HashMap<String, String>> mSensorItemList = new ArrayList<HashMap<String,String>>();
+    private List<Map<String, Object>> mSensorItemList = new ArrayList<Map<String,Object>>();
+
+    private HashMap<String, Integer> mSensorMap = new HashMap<String, Integer>();
+
+    private HashMap<String, Integer> mSensorBackup = new HashMap<String, Integer>();
 
     public SensorsFragment() {
     }
@@ -60,13 +63,24 @@ public class SensorsFragment extends Fragment implements SensorEventListener {
 
         mSensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
 
-        for (Sensor sensor : mSensorList) {
-            HashMap<String, String> map = new HashMap<String, String>();
-            map.put(Constants.LIST_NAME, sensor.getName());
-            map.put(Constants.LIST_VALUE, "");
-            mSensorItemList.add(map);
-            LogUtils.d(SensorsFragment.class, "sensor name : " + sensor.getName() + " ("+sensor.getVendor()+")");
-            LogUtils.d(SensorsFragment.class, "sensor power : " + sensor.getPower());
+        for (int i=0;i<mSensorList.size();i++) {
+            Sensor sensor = mSensorList.get(i);
+            if (!mSensorMap.containsKey(sensor.getName())) {
+                HashMap<String, Object> map = new HashMap<String, Object>();
+                map.put(Constants.LIST_NAME, sensor.getName() + " ("+sensor.getVendor()+")");
+                map.put(Constants.LIST_VALUE, "");
+                mSensorItemList.add(map);
+                mSensorMap.put(sensor.getName(), mSensorItemList.size() - 1);
+
+                LogUtils.d(SensorsFragment.class, "sensor name : " + sensor.getName() + " ("+sensor.getVendor()+")");
+                LogUtils.d(SensorsFragment.class, "sensor power : " + sensor.getPower());
+            } else {
+                HashMap<String, Object> map = new HashMap<String, Object>();
+                map.put(Constants.LIST_NAME, sensor.getName() + " ("+sensor.getVendor()+")");
+                map.put(Constants.LIST_VALUE, "");
+                mSensorItemList.add(map);
+                mSensorBackup.put(sensor.getName(), mSensorItemList.size() - 1);
+            }
         }
         super.onAttach(activity);
     }
@@ -75,8 +89,7 @@ public class SensorsFragment extends Fragment implements SensorEventListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_sensors, container, false);
         ListView sensor_list = (ListView)rootView.findViewById(R.id.sensor_list);
-        getData();
-        mAdapter = new SimpleAdapter(getActivity(), mList, R.layout.sensor_info_item, new String[] {Constants.LIST_NAME, Constants.LIST_VALUE},
+        mAdapter = new SimpleAdapter(getActivity(), mSensorItemList, R.layout.sensor_info_item, new String[] {Constants.LIST_NAME, Constants.LIST_VALUE},
                 new int[] { R.id.sensor_name, R.id.sensor_value});
         sensor_list.setAdapter(mAdapter);
         return rootView;
@@ -88,10 +101,6 @@ public class SensorsFragment extends Fragment implements SensorEventListener {
         }
     }
 
-    protected List<Map<String, Object>> getData() {
-        return null;
-    }
-
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // TODO Auto-generated method stub
@@ -100,5 +109,163 @@ public class SensorsFragment extends Fragment implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        int index = mSensorMap.get(sensor.getName());
+        Map<String, Object> map = mSensorItemList.get(index);
+        StringBuilder builder = new StringBuilder();
+        switch (sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                float x_accelerometer = event.values[0];
+                float y_accelerometer = event.values[1];
+                float z_accelerometer = event.values[2];
+                builder.append(getActivity().getString(R.string.sensor_xyz_acceleration, x_accelerometer, y_accelerometer, z_accelerometer));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                boolean use_celsius = BatteryUtils.getUseCelsius(getActivity());
+                float ambient_temperature = event.values[0];
+                if (use_celsius) {
+                    builder.append(getActivity().getString(R.string.sensor_temperature_celsius, ambient_temperature));
+                } else {
+                    ambient_temperature = (float)(ambient_temperature * 1.8 + 32);
+                    builder.append(getActivity().getString(R.string.sensor_temperature_fahrenheit, ambient_temperature));
+                }
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_GAME_ROTATION_VECTOR:
+                float x_game_rotation = event.values[0];
+                float y_game_rotation = event.values[1];
+                float z_game_rotation = event.values[2];
+                builder.append(getActivity().getString(R.string.sensor_xyz, String.format("%.5f", x_game_rotation), String.format("%.5f", y_game_rotation), String.format("%.5f", z_game_rotation)));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR:
+                float x_geomagnetic_rotation = event.values[0];
+                float y_geomagnetic_rotation = event.values[1];
+                float z_geomagnetic_rotation = event.values[2];
+                builder.append(getActivity().getString(R.string.sensor_xyz, String.format("%.5f", x_geomagnetic_rotation), String.format("%.5f", y_geomagnetic_rotation), String.format("%.5f", z_geomagnetic_rotation)));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_GRAVITY:
+                float x_gravity = event.values[0];
+                float y_gravity = event.values[1];
+                float z_gravity = event.values[2];
+                builder.append(getActivity().getString(R.string.sensor_xyz_acceleration, x_gravity, y_gravity, z_gravity));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_GYROSCOPE:
+                float x_gyroscore = event.values[0];
+                float y_gyroscore = event.values[1];
+                float z_gyroscore = event.values[2];
+                builder.append(getActivity().getString(R.string.sensor_xyz_gyroscore, x_gyroscore, y_gyroscore, z_gyroscore));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_GYROSCOPE_UNCALIBRATED:
+                if (mSensorBackup.containsKey(sensor.getName())) {
+                    map = mSensorItemList.get(mSensorBackup.get(sensor.getName()));
+                }
+                float x_gyroscore_uncalibrated = event.values[0];
+                float y_gyroscore_uncalibrated = event.values[1];
+                float z_gyroscore_uncalibrated = event.values[2];
+
+                float x_estimated_drift = event.values[3];
+                float y_estimated_drift = event.values[4];
+                float z_estimated_drift = event.values[5];
+                builder.append(getActivity().getString(R.string.sensor_xyz_gyroscore, x_gyroscore_uncalibrated, y_gyroscore_uncalibrated, z_gyroscore_uncalibrated));
+                builder.append("\n");
+                builder.append(getActivity().getString(R.string.sensor_xyz_gyroscore_estimated_drift, x_estimated_drift, y_estimated_drift, z_estimated_drift));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_LIGHT:
+                float light = event.values[0];
+                builder.append(getActivity().getString(R.string.sensor_light, light));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_LINEAR_ACCELERATION:
+                float x_linear_accelerometer = event.values[0];
+                float y_linear_accelerometer = event.values[1];
+                float z_linear_accelerometer = event.values[2];
+                builder.append(getActivity().getString(R.string.sensor_xyz_acceleration, x_linear_accelerometer, y_linear_accelerometer, z_linear_accelerometer));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                float x_magnetic_field = event.values[0];
+                float y_magnetic_field = event.values[1];
+                float z_magnetic_field = event.values[2];
+                builder.append(getActivity().getString(R.string.sensor_xyz_magnetic_field, x_magnetic_field, y_magnetic_field, z_magnetic_field));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
+                if (mSensorBackup.containsKey(sensor.getName())) {
+                    map = mSensorItemList.get(mSensorBackup.get(sensor.getName()));
+                }
+                float x_uncali = event.values[0];
+                float y_uncali = event.values[1];
+                float z_uncali = event.values[2];
+
+                float x_bias = event.values[3];
+                float y_bias = event.values[4];
+                float z_bias = event.values[5];
+                builder.append(getActivity().getString(R.string.sensor_xyz_magnetic_field, x_uncali, y_uncali, z_uncali));
+                builder.append("\n");
+                builder.append(getActivity().getString(R.string.sensor_xyz_magnetic_field_bias, x_bias, y_bias, z_bias));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_ORIENTATION:
+                float azimuth = event.values[0];
+                float pitch = event.values[1];
+                float roll = event.values[2];
+                builder.append(getActivity().getString(R.string.sensor_orientation, azimuth, pitch, roll));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_PRESSURE:
+                float pressure = event.values[0];
+                builder.append(getActivity().getString(R.string.sensor_pressure, pressure));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                float distance = event.values[0];
+                builder.append(getActivity().getString(R.string.sensor_proximity, distance));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_RELATIVE_HUMIDITY:
+                float relative_humidity = event.values[0];
+                builder.append(getActivity().getString(R.string.sensor_relative_humidity, relative_humidity));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_ROTATION_VECTOR:
+                float x_rotation = event.values[0];
+                float y_rotation = event.values[1];
+                float z_rotation = event.values[2];
+                builder.append(getActivity().getString(R.string.sensor_xyz, String.format("%.5f", x_rotation), String.format("%.5f", y_rotation), String.format("%.5f", z_rotation)));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_SIGNIFICANT_MOTION:
+                break;
+            case Sensor.TYPE_STEP_COUNTER:
+                float step_offset = event.values[0];
+                builder.append(getActivity().getString(R.string.sensor_step, step_offset));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_STEP_DETECTOR:
+                float step_detector = event.values[0];
+                builder.append(getActivity().getString(R.string.sensor_step, step_detector));
+                builder.append("\n");
+                break;
+            case Sensor.TYPE_TEMPERATURE:
+                boolean is_use_celsius = BatteryUtils.getUseCelsius(getActivity());
+                float temperature = event.values[0];
+                if (is_use_celsius) {
+                    builder.append(getActivity().getString(R.string.sensor_temperature_celsius, temperature));
+                } else {
+                    temperature = (float)(temperature * 1.8 + 32);
+                    builder.append(getActivity().getString(R.string.sensor_temperature_fahrenheit, temperature));
+                }
+                builder.append("\n");
+                break;
+        }
+        builder.append(getActivity().getString(R.string.sensor_current_consumption, sensor.getPower()));
+        map.put(Constants.LIST_VALUE, builder.toString());
+        notifyDataSetChanged();
     };
 }
